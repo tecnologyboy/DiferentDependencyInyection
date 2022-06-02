7ca9ccfae15c0f855ae7f672e6958430488f22aa
tecnologyboy <lps76@hotmail.com>
2022-06-02 00:07:38 -0400

    Explicacion de inyeccion de dependencias y ciclo de vida de las inyecciones

---
+++ MvcDiferentDependencyInyection/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcDiferentDependencyInyection.Models;
+using System.Diagnostics;
+
+namespace MvcDiferentDependencyInyection.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ISingleton _silgleton;
+
+        private readonly ITransient _transient;
+
+        private readonly IScoped _scoped;
+
+        private readonly ISingleton _silgleton2;
+
+        private readonly ITransient _transient2;
+
+        private readonly IScoped _scoped2;
+
+
+        public HomeController(ISingleton silgleton, ITransient itransient, IScoped scoped, ISingleton silgleton2, ITransient itransient2, IScoped scoped2)
+        {
+            _silgleton = silgleton;
+            _transient = itransient;
+            _scoped = scoped;
+
+
+            _silgleton2 = silgleton2;
+            _transient2 = itransient2;
+            _scoped2 = scoped2;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.singleton = _silgleton;
+            ViewBag.transient = _transient;
+            ViewBag.scoped = _scoped;
+
+            ViewBag.singleton2 = _silgleton2;
+            ViewBag.transient2 = _transient2;
+            ViewBag.scoped2 = _scoped2;
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MvcDiferentDependencyInyection/MyObject.cs
@@ -0,0 +1,35 @@
+﻿namespace MvcDiferentDependencyInyection
+{
+    public interface IScoped
+    {
+        
+    }
+
+    public interface ITransient
+    {
+        
+    }
+
+    public interface ISingleton
+    {
+        
+    }
+
+    public class MyObject: ISingleton, ITransient, IScoped
+    {
+        //public readonly int value;
+
+        //public MyObject()
+        //{
+        //    value = new Random().Next(10000);
+        //}
+
+
+        public readonly char value;
+
+        public MyObject()
+        {
+            value = Convert.ToChar(new Random().Next(97,122));
+        }
+    }
+}
